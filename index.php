c4a385a2b99913e3d185b4e630f2d1b281392b21
LDM VM <jaimelacabexgonzalo@gmai.com>
2021-01-13 13:39:03 +0100

    ayuda
    PFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF quierio morir

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang='es' dir='ltr'>
+
+<head>
+  <meta charset='utf-8'>
+  <title>JuegAlmi</title>
+  <link rel='stylesheet' type='text/css' href='css/style.css'>
+  <link rel='stylesheet' type='text/css' href='css/comun.css'>
+  <link href='https://fonts.googleapis.com/css2?family=Dancing+Script&display=swap' rel='stylesheet'>
+  <?php
+  include("datos.php");
+  ?>
+</head>
+<body>
+  
+</body>
